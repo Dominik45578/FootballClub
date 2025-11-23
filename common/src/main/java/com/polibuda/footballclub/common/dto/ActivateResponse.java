8264fb6d62319cf5523c2b052bc87f92b7237125
Dominik <dkkd3046@gmail.com>
2025-11-23 20:38:02 +0100

    Fixing my fu**ing stupid mistake

---
+++ common/src/main/java/com/polibuda/footballclub/common/dto/ActivateResponse.java
@@ -0,0 +1,22 @@
+package com.polibuda.footballclub.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActivateResponse {
+    
+    private boolean success;
+    
+    private String message;
+    
+    @Builder.Default
+    private LocalDateTime timestamp = LocalDateTime.now();
+}
---
+++ common/src/main/java/com/polibuda/footballclub/common/dto/LoginRequest.java
@@ -0,0 +1,22 @@
+package com.polibuda.footballclub.common.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+
+    @NotBlank(message = "email is required")
+    @Email
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
